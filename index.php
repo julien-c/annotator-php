fa99ebb9da66b91811c50861b9c19e7fc3d21106
Julien Chaumond <chaumond@gmail.com>
2013-01-24 12:15:12 +0100

    Implement root endpoint

--- index.php
+++ index.php
@@ -6,6 +6,17 @@ require_once __DIR__.'/vendor/autoload.php';
 $app = new Silex\Application();
 
 
+
+$app->get('/', function () use ($app) {
+	$out = array(
+		'name' => "Annotator Store API (PHP)",
+		'version' => '1.0.0',
+		'author' => 'julien-c'
+	);
+	return $app->json($out);
+});
+
+
 $app->get('/annotations', function () use ($app) {
 	$out = array();
 	return $app->json($out);
