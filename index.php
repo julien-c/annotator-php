7cf862f16d873bedf963cf3f43d3e24a747a31ff
Julien Chaumond <chaumond@gmail.com>
2013-01-24 12:35:49 +0100

    Implement `create` endpoint (with workaround)

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
 
 require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application();
 
+/***
+ *
+ * Accepting JSON in request body.
+ * @note: the method described in http://silex.sensiolabs.org/doc/cookbook/json_request_body.html doesn't allow us to get the whole parameter array.
+ *
+ */
+
+$app->before(function (Request $request) use ($app) {
+	if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+		$app['data'] = json_decode($request->getContent(), true);
+	}
+});
+
+
+/***
+ *
+ * Endpoints.
+ * @see https://github.com/okfn/annotator/wiki/Storage
+ *
+ */
 
 
 $app->get('/', function () use ($app) {
@@ -20,6 +41,19 @@ $app->get('/', function () use ($app) {
 $app->get('/annotations', function () use ($app) {
 	$out = array();
 	return $app->json($out);
+});
+
+
+$app->post('/annotations', function () use ($app) {
+	$post = $app['data'];
+	
+	$m = new Mongo();
+	$m->annotator->annotations->insert($post, array('safe' => true));
+	
+	$post['id'] = (string) $post['_id'];
+	unset($post['_id']);
+	
+	return $app->json($post);
 });
 
 
